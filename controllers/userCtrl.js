8a5c7119764c2f1b5fd042d258896d38813d6d2e
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2021-05-31 22:24:00 +0530

    password checking added

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -2,10 +2,28 @@ const Users = require('../models/userModels')
 
 
 const userCtrl = {
-    
-    register: (req,res)=>{
-        res.json({msg: "Test Controllers"})
+
+    register: async (req,res) =>{
+        try {
+            const {name, email, password} = req.body;
+
+            const user = await Users.findOne({email})
+
+            if(user){return res.status(400).json({msg: "The email already exists."})}
+
+            if(password.length < 6){
+                return res.status(400).json({msg: "The password must contain at-least 6 characters"})
+            }
+
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
     }
+    
+    // register: (req,res)=>{
+    //     res.json({msg: "Test Controllers"})
+    // }
     
     
     // register: async (req,res) =>{
